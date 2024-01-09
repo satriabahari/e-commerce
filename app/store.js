8b84a6c9169386dfd5456fd81f5437054d4a5849
Satria Bahari <satriaabaharii@gmail.com>
2024-01-10 01:27:31 +0700

    add some store : filter and product

--- app/store.js
+++ app/store.js
@@ -1,8 +1,12 @@
-import cartSlice from '@/features/cart/cartSlice'
-import { configureStore } from '@reduxjs/toolkit'
+import cartSlice from "@/features/cart/cartSlice";
+import filterSlice from "@/features/productList/filterSlice";
+import productSlice from "@/features/productList/productSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
 export default configureStore({
   reducer: {
-    cart: cartSlice.reducer
+    cart: cartSlice,
+    filter: filterSlice,
+    product: productSlice,
   },
-})
+});
