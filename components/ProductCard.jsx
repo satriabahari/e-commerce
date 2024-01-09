6d13f725d451f026fc20b7fbe5a8097ecba487e8
Satria Bahari <satriaabaharii@gmail.com>
2024-01-10 01:31:16 +0700

    fix code ProductCard.jsx

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -2,7 +2,7 @@
 
 import { addItemToCart } from "@/features/cart/cartSlice";
 import Image from "next/image";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const ProductCard = ({
   product,
@@ -14,20 +14,29 @@ const ProductCard = ({
   rating,
 }) => {
   const dispatch = useDispatch();
-  const currentItem = useSelector((state) => state.cart.items);
 
   const handleClickBuy = (product) => {
     dispatch(addItemToCart(product));
   };
 
   return (
-    <div className="border rounded-xl flex flex-col items-center text-center gap-y-4 p-4">
-      <Image src={image} alt={title} width={75} height={75} />
-      <div className="uppercase bg-gray-50 drop-shadow-lg p-4 rounded-md">
-        <h5 className="text-xs text-gray-400">{category}</h5>
-        <h1 className="text-sm text-gray-800">{title}</h1>
+    <div className="border rounded-xl flex flex-col justify-between items-center text-center gap-y-4 py-6 px-4 border-gray-400">
+      <div className="bg-cover bg-center w-28 h-28">
+        <Image
+          src={image}
+          alt={title}
+          width={75}
+          height={75}
+          className="w-full h-full"
+        />
       </div>
-      <h3 className="text-xs text-gray-500">{description}</h3>
+      <div className=" bg-gray-50 drop-shadow-lg p-4 rounded-md">
+        <h5 className="text-xs text-gray-400 capitalize mb-2">{category}</h5>
+        <h1 className="text-xs text-gray-800 uppercase">
+          {title.length > 35 ? title.substring(0, 35) + "..." : title}
+        </h1>
+      </div>
+      {/* <h3 className="text-xs text-gray-500">{description}</h3> */}
       <h2 className=" font-semibold">${price}</h2>
       <h3 className="text-xs text-gray-500">
         {rating.rate} | {rating.count} reviews
