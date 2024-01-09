aefb97447e3565064ee9851b97f9e2793680d6a7
Satria Bahari <satriaabaharii@gmail.com>
2024-01-10 01:33:07 +0700

    Add component button filter

---
+++ components/ButtonFilters.jsx
@@ -0,0 +1,7 @@
+const ButtonFilters = ({handleOpenModalFilters}) => {
+  return (
+    <button className="bg-blue-500 rounded-full w-10 h-10" onClick={handleOpenModalFilters}>!</button>
+  )
+}
+
+export default ButtonFilters
