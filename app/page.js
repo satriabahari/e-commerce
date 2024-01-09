0b5545c51a507f435f7cc68e97545caa6494aa84
Satria Bahari <satriaabaharii@gmail.com>
2024-01-10 01:29:30 +0700

    fixed some code

--- app/page.js
+++ app/page.js
@@ -1,13 +1,50 @@
+"use client";
+
 import Header from "@/components/Header";
-import CartModal from "@/components/Modal";
+import Modal from "@/components/Modal";
 import ProductList from "@/components/ProductList";
+import { useState } from "react";
+import CartModal from "@/components/CartModal";
+import ButtonFilters from "@/components/ButtonFilters";
+import FiltersModal from "@/components/FiltersModal";
 
 export default function Home() {
+  const [cartModalIsOpen, setCartModalIsOpen] = useState(false);
+  const [filtersModalIsOpen, setFilterModalIsOpen] = useState(false);
+
+  const handleOpenModalCart = () => {
+    setCartModalIsOpen(true);
+  };
+
+  const handleCloseModalCart = () => {
+    setCartModalIsOpen(false);
+  };
+
+  const handleOpenModalFilters = () => {
+    setFilterModalIsOpen(true);
+  };
+
+  const handleCloseModalFilters = () => {
+    setFilterModalIsOpen(false);
+  };
+
   return (
     <main>
-      <Header/>
+      <Header handleOpenModalCart={handleOpenModalCart} />
+      <ButtonFilters handleOpenModalFilters={handleOpenModalFilters} />
       <ProductList />
-      <CartModal/>
+
+      {cartModalIsOpen ? (
+        <Modal>
+          <CartModal handleCloseModalCart={handleCloseModalCart} />
+        </Modal>
+      ) : null}
+      
+      {filtersModalIsOpen ? (
+        <Modal>
+          <FiltersModal handleCloseModalFilters={handleCloseModalFilters} />
+        </Modal>
+      ) : null}
     </main>
   );
 }
